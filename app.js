a3926d3126eba37c20eb1347ef43ebcc0b357b61
Zachary Satoshi Feldcamp <zachary.feldcamp@me.com>
2016-08-31 17:06:09 -0400

    Serve Polymer from Express

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+    // res.send('Hello World!');
+// });
+
+app.use(express.static('./build/unbundled/'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000');
